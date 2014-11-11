d371a92f7cb1558ac48e5911f4bda28953c0cfa2
Andrew Stern <andrew.stern@gobbler.com>
2014-11-11 14:02:51 -0800

    build

--- build/multiarity.js
+++ build/multiarity.js
@@ -6,35 +6,35 @@
  * Licensed under the MIT license.
  */
 'use strict';
-var _$1046 = require('lodash');
+var _$1140 = require('lodash');
 module.exports = function multiArity() {
     if (arguments.length === 0) {
         throw new Error('multiarity expects at least 1 arument');
     }
-    var args$1049 = _$1046.toArray(arguments), arities$1050 = {}, ctx$1051 = { recur: callByArity$1053 }, opts$1052 = _$1046.first(args$1049);
-    if (_$1046.isPlainObject(opts$1052)) {
-        if (!isAllFns$1048(_$1046.values(opts$1052))) {
+    var args$1143 = _$1140.toArray(arguments), arities$1144 = {}, ctx$1145 = { recur: callByArity$1147 }, opts$1146 = _$1140.first(args$1143);
+    if (_$1140.isPlainObject(opts$1146)) {
+        if (!isAllFns$1142(_$1140.values(opts$1146))) {
             throw new Error('multiarity vals must be type function');
         }
-        _$1046.extend(arities$1050, opts$1052);
+        _$1140.extend(arities$1144, opts$1146);
     } else {
-        _$1046.extend(arities$1050, arityMapForFunctions$1047(args$1049));
+        _$1140.extend(arities$1144, arityMapForFunctions$1141(args$1143));
     }
-    return callByArity$1053;
-    function callByArity$1053() {
-        var args$1054 = _$1046.toArray(arguments), fn_len$1055 = args$1054.length, fn_for_arity$1056 = _$1046.has(arities$1050, fn_len$1055) ? arities$1050[fn_len$1055] : arities$1050['otherwise'];
-        if (_$1046.isUndefined(fn_for_arity$1056)) {
-            throw new Error('wrong number of args: ' + fn_len$1055);
+    return callByArity$1147;
+    function callByArity$1147() {
+        var args$1148 = _$1140.toArray(arguments), fn_len$1149 = args$1148.length, fn_for_arity$1150 = _$1140.has(arities$1144, fn_len$1149) ? arities$1144[fn_len$1149] : arities$1144['otherwise'];
+        if (_$1140.isUndefined(fn_for_arity$1150)) {
+            throw new Error('wrong number of args: ' + fn_len$1149);
         }
-        return fn_for_arity$1056.apply(ctx$1051, args$1054);
+        return fn_for_arity$1150.apply(ctx$1145, args$1148);
     }
 };
-function arityMapForFunctions$1047(fns_list$1057) {
-    return _$1046.zipObject(_$1046.map(fns_list$1057, function (_1$1059) {
-        return _1$1059.length;
-    }), fns_list$1057);
+function arityMapForFunctions$1141(fns_list$1151) {
+    return _$1140.zipObject(_$1140.map(fns_list$1151, function (_1$1153) {
+        return _1$1153.length;
+    }), fns_list$1151);
 }
-function isAllFns$1048(arr$1060) {
-    return _$1046.all(arr$1060, _$1046.isFunction);
+function isAllFns$1142(arr$1154) {
+    return _$1140.all(arr$1154, _$1140.isFunction);
 }
 //# sourceMappingURL=multiarity.js.map
